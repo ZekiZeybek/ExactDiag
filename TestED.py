d03eb68b1e6eb6d5d1886ad06faf864c8746c439
Zeki Zeybek <zekiizeybek@gmail.com>
2020-04-21 23:21:35 +0300

    Update TestED.py

--- TestED.py
+++ TestED.py
@@ -1,19 +1,33 @@
 import ED
 from scipy.sparse.linalg import eigs, eigsh
 
-# MAIN PROGRAM
-h = ED.Hamiltonian(4, 2, 1, 1,2)
-h2 = ED.Hamiltonian(4,1,1,1,2)
-A = h2.H_int()
-B = h2.H_kin(h2.lattice2D())
-D = (A.tocsr() + B.tocsr())
+#Example Case: Single Particle on a 2 X 2 square lattice
 
-print(h2.lattice2D())
+#Initialize Hamiltonian object with required parameters w.r.t the given order
+# (M, N, T, U, lac(required only for 2D case) ) in which
+# M = # of sites, N = # of particles, T = hopping strength, U = interaction strength
+# lac = this parameter is only required when setting up 2D lattice in our case it is 2 since
+# we are dealing with 2 X 2 square lattice amounting to 4 sites
+H = ED.Hamiltonian(4, 1, 1, 1, 2)
 
-#vals,vecs = eigs(D, 1, sigma=2)
+#Forming interaction hamiltonian and hopping hamiltonian
+H_int = H.H_int()
+H_kin = H.H_kin(H.lattice2D()) #!!! In 1D case make sure to use lattice1D() function and leave empty the
+                               # lac parameter above
 
-valss, vecss = eigsh(D, k=3)
+H_tot = (H_int.tocsr() + H_kin.tocsr())
 
-#print(vals)
-print()
-print(valss)
+#One can compute the dimension of hilbert space so that the Lanczos can look for such number of eigenvalues
+print(H.D()) #It will result in 4 in our case, we configured the functions below to look for 2 eigenvalues
+
+#This method is more efficient for computing low lying eigenvalues :
+eig_val,eig_vecs = eigs(H_tot, 2, sigma=-5) # Looking for 2 eigenvalues the arg < H.D() condition must be satisfied!
+
+#More generic way w/o any specification of the spectrum
+eig_val2, eig_vecs2 = eigsh(H_tot, k=2) # Looking for 2 eigenvalues: k < H.D() condition must be satisfied!
+
+
+#Printing computed eigenvalues
+print(ED.np.around(eig_val, decimals=3))
+print(ED.np.around(eig_val2, decimals=3))
+
